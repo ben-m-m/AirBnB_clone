cc6c07eb62bf78a223103f560b1b82b1b96a2f62
Emmanuel Munubi Mwangi <manumunubi@gmail.com>
2023-05-12 12:26:07 +0300

    simple constructor - passes a dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,33 @@
 #!/usr/bin/python3
 """ Base model class """
 
+from uuid import uuid4
+import datetime
+
 
 class BaseModel:
     """ Base model class """
-    pass
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+        
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+        
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
