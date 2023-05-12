2af9b067da1e2b2cb41ae7a3f63c0e1e3e25d6dd
Emmanuel Munubi Mwangi <manumunubi@gmail.com>
2023-05-12 13:10:51 +0300

    pep8 validation and documentation

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,29 @@ from engine import storage
 
 class BaseModel:
     """ Base model class """
+
     def __init__(self, *args, **kwargs):
+        """construct a BaseModel
+        Args:
+            args (tuple): unused
+            kwargs (dict): key/value pairs of attributes
+
+        attributes:
+            id (str): unique id generated
+            created_at (datetime): time of creation
+            updated_at (datetime): time of last update
+
+        description:
+            if kwargs is not empty, the instance is created from a
+            dictionary of keys/values
+            otherwise, the instance is created with a unique id and
+            the current datetime - timestamped, and added to storage        
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime.datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
                 elif key != "__class__":
                     setattr(self, key, value)
         else:
@@ -20,15 +38,18 @@ class BaseModel:
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
             storage.new(self)
-        
+
     def __str__(self):
+        """returns a string representation of the instance"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+
     def save(self):
+        """updates the public instance attribute updated_at"""
         self.updated_at = datetime.datetime.now()
         storage.save()
-        
+
     def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" import file_storage variable """
 import file_storage
 
 storage = file_storage.FileStorage()
